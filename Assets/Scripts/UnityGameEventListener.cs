b9d3647ec0c7b76aff7471230c41bd4a5bdafc43
antek1085 <antek1085@o2.pl>
2023-04-21 17:21:24 +0200

    player fix

---
+++ Assets/Scripts/UnityGameEventListener.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityGameEventListener : MonoBehaviour,IGameEventListener
+{
+    public GameEvent gameEvent;
+
+    public UnityEvent response;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameEvent.RegisterListener(this);
+    }
+
+    // Update is called once per frame
+    void OnDestroy()
+    {
+        gameEvent.RegisterListener(this);
+    }
+    public void OnEventRaise()
+    {
+        response?.Invoke();
+    }
+}
