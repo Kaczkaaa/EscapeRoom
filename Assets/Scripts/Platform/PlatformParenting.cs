ccfb541c08f4c5bf68ca0c19a307294c6896b783
Jakub Jóskowiak <jjoskowiak@edu.cdv.pl>
2023-05-11 17:11:55 +0200

    working moving platform

---
+++ Assets/Scripts/Platform/PlatformParenting.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformParenting : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.SetParent(null);
+    }
+}
